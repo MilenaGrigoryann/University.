a7d222d09b714e283514ac2f085418a220d1f6fa
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-16 19:12:41 +0400

    Add files via upload

---
+++ BL/BaseManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UniversityConsoleApp1.Models;
+
+namespace UniversityConsoleApp1.BL
+{
+    public class BaseManager
+    {
+        int maxAge = 89;
+        public virtual Person Create(string firstName, string lastNmae, int age) => new Person(firstName, lastNmae, age);
+        public virtual Person[] Create(int count, int minAge)
+        {
+            Person[] people = new Person[count];
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                people[i] = new Person($"name{i}", $" lastName{i}", rnd.Next(minAge, maxAge));
+            }
+
+            return people;
+        }
+        public virtual void Print(Person person)
+        {
+            Console.WriteLine("**********Person**********");
+            Console.WriteLine($"id:{person.ID} name:{person.FirstName} lastName:{person.LastName} age:{person.Age}");
+
+        }
+        public virtual void Print(Person[] people)
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                Print(people[i]);
+            }
+        }
+    }
+}
